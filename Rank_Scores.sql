08a627e5383ab5265a80677a35ba2cec8dd9676c
John Narvaez <jpnar_99@yahoo.com>
2023-05-28 14:15:50 -0700

    Commit the Rank Scores

---
+++ Rank_Scores.sql
@@ -0,0 +1,86 @@
+/* Rank scores
+
+This is a problem code from Leetcode to combine 2 tables.
+Ranked as MEDIUM.
+Table: scores
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| score       | decimal |
++-------------+---------+
+id is the primary key for this table.
+Each row of this table contains the score of a game. score is a floating point value with two decimal places.
+ 
+
+Write an SQL query to rank the scores. The ranking should be calculated according to the following rules:
+
+The scores should be ranked from the highest to the lowest.
+If there is a tie between two scores, both should have the same ranking.
+After a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no holes between ranks.
+Return the result table ordered by score in descending order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+scores table:
++----+-------+
+| id | score |
++----+-------+
+| 1  | 3.50  |
+| 2  | 3.65  |
+| 3  | 4.00  |
+| 4  | 3.85  |
+| 5  | 4.00  |
+| 6  | 3.65  |
++----+-------+
+Output: 
++-------+------+
+| score | rank |
++-------+------+
+| 4.00  | 1    |
+| 4.00  | 1    |
+| 3.85  | 2    |
+| 3.65  | 3    |
+| 3.65  | 3    |
+| 3.50  | 4    |
++-------+------+
+
+*/
+
+DROP TABLE IF EXISTS scores;
+
+/* Create the scores Table */
+CREATE TABLE scores (id INT, score DECIMAL(10, 2),);
+
+/* Insert values in score Table */
+INSERT INTO scores (id, score)
+VALUES (1, 3.50),
+    (2, 3.65),
+    (3, 4.00),
+    (4, 3.85),
+    (5, 4.00),
+    (6, 3.65);
+
+/* Code Solution */
+SELECT score,
+    DENSE_RANK() OVER(
+        ORDER BY score DESC
+    ) AS rank
+FROM scores;
+
+-- To show only Distinct score ranking.
+SELECT DISTINCT(score),
+    DENSE_RANK() OVER(
+        ORDER BY score DESC
+    ) AS rank
+FROM scores
+ORDER BY rank;
+
+SELECT score
+FROM scores;
