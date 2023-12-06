4239ac21b53dcf338dcb75d7d20bcb40e6605ca3
John Narvaez <jpnar_99@yahoo.com>
2023-12-05 19:32:10 -0800

    Update Game_Play_Analysis_IV.sql
    Fix code formatting.

--- Game_Play_Analysis_IV.sql
+++ Game_Play_Analysis_IV.sql
@@ -46,18 +46,18 @@ USE leetcode_sql;
 DROP TABLE IF EXISTS Activity;
 CREATE TABLE Activity (
 	player_id INT,
-    device_id INT,
-    event_date DATE,
-    game_played INT
+    	device_id INT,
+    	event_date DATE,
+   	game_played INT
 )
 ;
 
 -- Insert values to Activity table
 INSERT INTO Activity (
 	player_id,
-    device_id,
-    event_date,
-    game_played
+    	device_id,
+    	event_date,
+    	game_played
     )
 VALUES
 	(1,	2,	'2016-03-01',	5),   
@@ -72,7 +72,7 @@ SELECT * FROM Activity;
 
 /* 1st step Solution */
 SELECT
-	player_id,
+    player_id,
     MIN(event_date) AS first_log
 FROM Activity
 GROUP BY player_id
@@ -103,17 +103,17 @@ JOIN
 ;
 /* 
 # player_id		event_date		first_log
-	1			2016-03-01		2016-03-01
-	1			2016-03-02		2016-03-01
-	2			2017-06-25		2017-06-25
-	3			2016-03-02		2016-03-02
-	3			2018-07-03		2016-03-02
+	1		2016-03-01		2016-03-01
+	1		2016-03-02		2016-03-01
+	2		2017-06-25		2017-06-25
+	3		2016-03-02		2016-03-02
+	3		2018-07-03		2016-03-02
 */
 
 /* Third add the AND DATEDIFF = 1 */
 SELECT 
-	-- COUNT(DISTINCT(t1.player_id)) AS num_first_log_players
-	t1.player_id,
+    -- COUNT(DISTINCT(t1.player_id)) AS num_first_log_players
+    t1.player_id,
     t1.event_date,
     t2.first_log
 FROM Activity AS t1
@@ -154,14 +154,14 @@ SELECT
 
 /* Alternative. Not tested on Leetcode*/
 CREATE VIEW temp_table AS 
-SELECT t1.player_id,
-		t1.first_log,
+SELECT  t1.player_id,
+	t1.first_log,
         t1.event_date,
-	   DATEDIFF(t1.event_date, t1.first_log) AS date_diff
+	DATEDIFF(t1.event_date, t1.first_log) AS date_diff
 FROM 
 	(SELECT
 		player_id,
-        event_date,
+        	event_date,
 		MIN(event_date) OVER(PARTITION BY player_id) AS first_log
 	FROM Activity) AS t1
 -- WHERE DATEDIFF(t1.event_date, t1.first_log) = 1 -- Uncomment if want only date_diff with 1 only.
@@ -169,18 +169,21 @@ FROM
 -- DROP VIEW temp_table;
 /*
 # player_id		first_log		event_date		date_diff
-1				2016-03-01		2016-03-01			0
-1				2016-03-01		2016-03-02			1
-2				2017-06-25		2017-06-25			0
-3				2016-03-02		2016-03-02			0
-3				2016-03-02		2018-07-03			853
+1			2016-03-01		2016-03-01		  0
+1			2016-03-01		2016-03-02		  1
+2			2017-06-25		2017-06-25		  0
+3			2016-03-02		2016-03-02		  0
+3			2016-03-02		2018-07-03		853
 */
 
-SELECT (ROUND (
-	(SELECT COUNT(*) from temp_table)
-	/ 
-    (SELECT COUNT(DISTINCT(player_id)) 
-    FROM activity),2)) AS fraction;
+SELECT (
+	ROUND (
+		(SELECT COUNT(*) 
+		  FROM temp_table)
+		/ 
+    		(SELECT COUNT(DISTINCT(player_id)) 
+    		 FROM activity),2)
+	        ) AS fraction;
 ;
 
 /* 
@@ -194,7 +197,8 @@ Output:
 /* Other solution. Solution using SELF JOIN - Only passed one test case */
 SELECT 
 	ROUND( a1.player_id
-	/ (SELECT COUNT(DISTINCT(player_id)) FROM Activity), 2) AS fraction
+	/ (SELECT COUNT(DISTINCT(player_id)) FROM Activity), 2
+	) AS fraction
 FROM Activity AS a1
 JOIN Activity AS a2
 	ON a1.player_id = a2.player_id AND a1.device_id = a2.device_id
